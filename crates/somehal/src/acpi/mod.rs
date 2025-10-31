d212dd0eb5f804e951c0256b7881b596f3eddd95
周睿 <zrufo747@outlook.com>
2025-11-01 00:42:40 +0800

    feat: 重构ACPI处理逻辑，添加EFI主函数和Sierpinski三角形绘制功能

--- crates/somehal/src/acpi/mod.rs
+++ crates/somehal/src/acpi/mod.rs
@@ -1,122 +1,18 @@
+use core::ffi::c_void;
+
 use acpi::{Handle, Handler};
+
+static mut RSDP: usize = 0;
+
+pub(crate) fn set_rsdp(addr: *const c_void) {
+    unsafe {
+        RSDP = addr as usize;
+    }
+}
+
+fn rsdp() -> *const c_void {
+    unsafe { RSDP as _ }
+}
 
 #[derive(Clone)]
 struct AcpiHandle;
-
-impl Handler for AcpiHandle {
-    unsafe fn map_physical_region<T>(
-        &self,
-        physical_address: usize,
-        size: usize,
-    ) -> acpi::PhysicalMapping<Self, T> {
-        
-    }
-
-    fn unmap_physical_region<T>(region: &acpi::PhysicalMapping<Self, T>) {
-        todo!()
-    }
-
-    fn read_u8(&self, acpi::address: usize) -> u8 {
-        todo!()
-    }
-
-    fn read_u16(&self, acpi::address: usize) -> u16 {
-        todo!()
-    }
-
-    fn read_u32(&self, acpi::address: usize) -> u32 {
-        todo!()
-    }
-
-    fn read_u64(&self, acpi::address: usize) -> u64 {
-        todo!()
-    }
-
-    fn write_u8(&self, acpi::address: usize, value: u8) {
-        todo!()
-    }
-
-    fn write_u16(&self, acpi::address: usize, value: u16) {
-        todo!()
-    }
-
-    fn write_u32(&self, acpi::address: usize, value: u32) {
-        todo!()
-    }
-
-    fn write_u64(&self, acpi::address: usize, value: u64) {
-        todo!()
-    }
-
-    fn read_io_u8(&self, port: u16) -> u8 {
-        todo!()
-    }
-
-    fn read_io_u16(&self, port: u16) -> u16 {
-        todo!()
-    }
-
-    fn read_io_u32(&self, port: u16) -> u32 {
-        todo!()
-    }
-
-    fn write_io_u8(&self, port: u16, value: u8) {
-        todo!()
-    }
-
-    fn write_io_u16(&self, port: u16, value: u16) {
-        todo!()
-    }
-
-    fn write_io_u32(&self, port: u16, value: u32) {
-        todo!()
-    }
-
-    fn read_pci_u8(&self, acpi::address: acpi::PciAddress, offset: u16) -> u8 {
-        todo!()
-    }
-
-    fn read_pci_u16(&self, acpi::address: acpi::PciAddress, offset: u16) -> u16 {
-        todo!()
-    }
-
-    fn read_pci_u32(&self, acpi::address: acpi::PciAddress, offset: u16) -> u32 {
-        todo!()
-    }
-
-    fn write_pci_u8(&self, acpi::address: acpi::PciAddress, offset: u16, value: u8) {
-        todo!()
-    }
-
-    fn write_pci_u16(&self, acpi::address: acpi::PciAddress, offset: u16, value: u16) {
-        todo!()
-    }
-
-    fn write_pci_u32(&self, acpi::address: acpi::PciAddress, offset: u16, value: u32) {
-        todo!()
-    }
-
-    fn nanos_since_boot(&self) -> u64 {
-        todo!()
-    }
-
-    fn stall(&self, microseconds: u64) {
-        todo!()
-    }
-
-    fn sleep(&self, milliseconds: u64) {
-        todo!()
-    }
-
-    fn create_mutex(&self) -> Handle {
-        todo!()
-    }
-
-    fn acquire(&self, mutex: Handle, timeout: u16) -> Result<(), acpi::aml::AmlError> {
-        todo!()
-    }
-
-    fn release(&self, mutex: Handle) {
-        todo!()
-    }
-}
--- crates/somehal/src/efi_stub/mod.rs
+++ crates/somehal/src/efi_stub/mod.rs
@@ -1,14 +1,21 @@
-use core::fmt::Write;
+use core::{fmt::Write, mem};
 
 use uefi::{
+    Result,
+    boot::MemoryType,
+    mem::memory_map::MemoryMap,
     prelude::*,
-    proto::loaded_image::LoadedImage,
+    proto::{
+        console::{gop::GraphicsOutput, text::Output},
+        loaded_image::LoadedImage,
+        shell_params::ShellParameters,
+    },
     system::with_config_table,
     table::cfg::{ACPI_GUID, ACPI2_GUID, ConfigTableEntry},
 };
 use uefi_raw::table::system::SystemTable;
 
-use crate::arch::relocate;
+use crate::{acpi::set_rsdp, arch::relocate};
 
 pub mod pe;
 
@@ -27,11 +34,56 @@ pub unsafe extern "C" fn efi_pe_entry(
 
         crate::console::set_printer(&UefiPrinter);
 
+        if let Err(e) = efi_main() {
+            println!("EFI application error: {:?}", e);
+            return e.status();
+        }
+
+        if let Err(e) = draw_sierpinski() {
+            println!("Failed to draw Sierpinski triangle: {:?}", e);
+        } else {
+            println!("Sierpinski triangle drawn successfully.");
+        }
+
         crate::arch::entry::efi_kernel_prepare();
     }
 
     // 返回成功状态
     Status::SUCCESS
+}
+
+fn efi_main() -> Result {
+    find_acpi_rsdp();
+
+    let mem_map = boot::memory_map(MemoryType::LOADER_DATA)?;
+    for desc in mem_map.entries() {
+        println!("{desc:#x?}");
+    }
+
+    let h = boot::get_handle_for_protocol::<LoadedImage>()?;
+
+    let img = boot::open_protocol_exclusive::<LoadedImage>(h)?;
+
+    match img.load_options_as_cstr16() {
+        Ok(cmdline) => {
+            println!("Kernel command line: {}", cmdline);
+            system::with_stdout(|stdout| {
+                let _ = cmdline.as_str_in_buf(stdout);
+            });
+        }
+        Err(e) => {
+            println!("Failed to get load options as CStr16: {:?}", e);
+        }
+    }
+
+    Ok(())
+}
+
+fn draw_sierpinski() -> Result {
+    // Open graphics output protocol.
+    let gop_handle = boot::get_handle_for_protocol::<GraphicsOutput>()?;
+    let mut gop = boot::open_protocol_exclusive::<GraphicsOutput>(gop_handle)?;
+    Ok(())
 }
 
 struct UefiPrinter;
@@ -54,19 +106,19 @@ impl crate::console::Printer for UefiPrinter {
     }
 }
 
-fn get_acpi_rsdp() -> Option<*const u8> {
+fn find_acpi_rsdp() {
     with_config_table(|config_table| {
         for entry in config_table {
             if entry.guid == ACPI2_GUID {
                 // ACPI 2.0 RSDP (推荐)
                 println!("Found ACPI 2.0 RSDP at address: {:p}", entry.address);
-                return Some(entry.address as *const u8);
+
+                set_rsdp(entry.address);
             } else if entry.guid == ACPI_GUID {
                 // ACPI 1.0 RSDP (备选)
                 println!("Found ACPI 1.0 RSDP at address: {:p}", entry.address);
-                return Some(entry.address as *const u8);
+                set_rsdp(entry.address);
             }
         }
-        None
     })
 }
