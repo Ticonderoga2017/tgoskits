52a1dc4e665522310b4f39f34462ae78e8a3d285
周睿 <zrufo747@outlook.com>
2026-01-21 19:58:40 +0800

    ♻️ refactor(loongarch64): 移除未使用的 IRQ 初始化函数

--- platform/somehal/src/arch/loongarch64/mod.rs
+++ platform/somehal/src/arch/loongarch64/mod.rs
@@ -3,14 +3,6 @@ use crate::common::PlatOp;
 pub struct Plat;
 
 impl PlatOp for Plat {
-    fn init_irq_main() -> Result<(), anyhow::Error> {
-        Ok(())
-    }
-
-    fn init_irq_current_cpu() -> Result<(), anyhow::Error> {
-        Ok(())
-    }
-
     fn irq_set_enable(irq: rdrive::IrqId, enable: bool) {}
 
     fn systick_irq() -> rdrive::IrqId {
